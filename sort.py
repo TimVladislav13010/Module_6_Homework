3496e715de3250339c39772992bb4b045fe36da5
TimVladislav13010 <109065019+TimVladislav13010@users.noreply.github.com>
2022-10-20 18:11:52 +0300

    add funk sort

--- sort.py
+++ sort.py
@@ -1,17 +1,43 @@
-from pathlib import Path
+import os
 import re
 import sys
 
 
-def sorter(path):
-    path = Path(path)
-    for i in path.iterdir():
-        print(i.name)
+def sorter(path=sys.argv[1]):
+    for i in os.listdir(path): #iterator po papkah
+        print(i)
+        if i.endswith(".jpg") \
+                or i.endswith(".jpeg") \
+                or i.endswith(".png") \
+                or i.endswith(".svg"):
+            print(f"this is foto fille {i}")
+        if i.endswith(".avi") \
+                or i.endswith(".mp4") \
+                or i.endswith(".mov") \
+                or i.endswith(".mkv"):
+            print(f"this is video fille {i}")
+        if i.endswith(".doc")\
+                or i.endswith(".docx") \
+                or i.endswith(".txt") \
+                or i.endswith(".pdf") \
+                or i.endswith(".xlsx") \
+                or i.endswith(".pptx"):
+            print(f"this is documents fille {i}")
+        if i.endswith(".mp3") \
+                or i.endswith(".ogg") \
+                or i.endswith(".wav") \
+                or i.endswith(".amr"):
+            print(f"this is musiс fille {i}")
+        if i.endswith(".zip") \
+                or i.endswith(".gz") \
+                or i.endswith(".tar"):
+            print(f"this is archives fille {i}")
+        elif:
+            print(f"this is uknown fille {i}")
+        if os.path.isdir(os.path.join(path, i)): #yakscho papka to zahodumo v nei
+            if i == "audio" or i =="video" or i == "archives" or i == "documents" or i =="images":
+                continue #perevirka na papku
+            sorter(os.path.join(path, i)) #recursuvno vuklukaemo funk
 
 
-if __name__ == "__main__":
-    pass
-     # sorter(r"E:\goit\Python education\Homework\Module_6_Homework\test_folder")
-
-
-sorter(r"E:\goit\Python education\Homework\Module_6_Homework\test_folder")
+sorter()
