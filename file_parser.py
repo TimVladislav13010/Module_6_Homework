776204982314bf641e3b3ed1736251c5088312c0
TimVladislav13010 <109065019+TimVladislav13010@users.noreply.github.com>
2022-10-23 20:22:18 +0300

    file_parser.py and main.py

--- file_parser.py
+++ file_parser.py
@@ -29,6 +29,33 @@ ZIP_ARCHIVES = []
 GZ_ARCHIVES = []
 TAR_ARCHIVES = []
 
+JPEG_IMAGES_NAME = []
+JPG_IMAGES_NAME = []
+PNG_IMAGES_NAME = []
+SVG_IMAGES_NAME = []
+
+MP3_AUDIO_NAME = []
+OGG_AUDIO_NAME = []
+WAV_AUDIO_NAME = []
+AMR_AUDIO_NAME = []
+
+AVI_VIDEO_NAME = []
+MP4_VIDEO_NAME = []
+MOV_VIDEO_NAME = []
+MKV_VIDEO_NAME = []
+
+DOC_DOCUMENTS_NAME = []
+DOCX_DOCUMENTS_NAME = []
+TXT_DOCUMENTS_NAME = []
+PDF_DOCUMENTS_NAME = []
+XLSX_DOCUMENTS_NAME = []
+PPTX_DOCUMENTS_NAME = []
+
+MY_OTHER_NAME = []
+
+ZIP_ARCHIVES_NAME = []
+GZ_ARCHIVES_NAME = []
+TAR_ARCHIVES_NAME = []
 
 REGISTER_EXTENSIONS = {
     'JPEG': JPEG_IMAGES,
@@ -54,8 +81,32 @@ REGISTER_EXTENSIONS = {
     'TAR': TAR_ARCHIVES
 }
 
+REGISTER_EXTENSIONS_NAME = {
+    'JPEG': JPEG_IMAGES_NAME,
+    'PNG': PNG_IMAGES_NAME,
+    'JPG': JPG_IMAGES_NAME,
+    'SVG': SVG_IMAGES_NAME,
+    'MP3': MP3_AUDIO_NAME,
+    'OGG': OGG_AUDIO_NAME,
+    'WAV': WAV_AUDIO_NAME,
+    'AMR': AMR_AUDIO_NAME,
+    'AVI': AVI_VIDEO_NAME,
+    'MP4': MP4_VIDEO_NAME,
+    'MOV': MOV_VIDEO_NAME,
+    'MKV': MKV_VIDEO_NAME,
+    'DOC': DOC_DOCUMENTS_NAME,
+    'DOCX': DOCX_DOCUMENTS_NAME,
+    'TXT': TXT_DOCUMENTS_NAME,
+    'PDF': PDF_DOCUMENTS_NAME,
+    'XLSX': XLSX_DOCUMENTS_NAME,
+    'PPTX': PPTX_DOCUMENTS_NAME,
+    'ZIP': ZIP_ARCHIVES_NAME,
+    'GZ': GZ_ARCHIVES_NAME,
+    'TAR': TAR_ARCHIVES_NAME
+}
 
 FOLDERS = []
+FOLDERS_NAME = []
 EXTENSIONS = set()
 UNKNOWN = set()
 
@@ -73,6 +124,7 @@ def scan(folder: Path) -> None:
             # перевіряємо, щоб папка не була тією, в яку ми складаємо вже файли.
             if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
                 FOLDERS.append(item)
+                FOLDERS_NAME.append(item.name)
                 # скануємо цю вкладену папку - рекурсія
                 scan(item)
             # перейти до наступного елемента в сканованій папці
@@ -83,18 +135,20 @@ def scan(folder: Path) -> None:
         fullname = folder / item.name  # взяти повний шлях до файлу (folder / item.name)
         if not ext:  # якщо файл не має розширення додати до невідомих
             MY_OTHER.append(fullname)  # fullname
+            MY_OTHER_NAME.append(item.name)
         else:
             try:
                 # взяти список куди покласти повний шлях до файлу
                 container = REGISTER_EXTENSIONS[ext]
+                container_name = REGISTER_EXTENSIONS_NAME[ext]
                 EXTENSIONS.add(ext)
                 container.append(fullname)  # fullname
-
+                container_name.append(item.name)
             except KeyError:
                 # Якщо ми не реєстрували розширення у REGISTER_EXTENSIONS, то додати до іншого
                 UNKNOWN.add(ext)
                 MY_OTHER.append(fullname)  # fullname
-
+                MY_OTHER_NAME.append(item.name)
 
 if __name__ == '__main__':
 
@@ -138,31 +192,31 @@ if __name__ == '__main__':
     # print(f"Folders: {FOLDERS[::-1]}")
 
 
-    result = f'''Images jpeg: {[JPEG_IMAGES]}
-        \n\nImages jpg: {JPG_IMAGES}
-        \n\nImages svg: {SVG_IMAGES}
-        \n\nImages png: {PNG_IMAGES}
-        \n\nAudio mp3: {MP3_AUDIO}
-        \n\nAudio ogg: {OGG_AUDIO}
-        \n\nAudio wav: {WAV_AUDIO}
-        \n\nAudio amr: {AMR_AUDIO}
-        \n\nVideo avi: {AVI_VIDEO}
-        \n\nVideo mp4: {MP4_VIDEO}
-        \n\nVideo mov: {MOV_VIDEO}
-        \n\nVideo mkv: {MKV_VIDEO}
-        \n\nDokument doc: {DOC_DOCUMENTS}
-        \n\nDokument docx: {DOCX_DOCUMENTS}
-        \n\nDokument txt: {TXT_DOCUMENTS}
-        \n\nDokument pdf: {PDF_DOCUMENTS}
-        \n\nDokument xlsx: {XLSX_DOCUMENTS}
-        \n\nDokument pptx: {PPTX_DOCUMENTS}
-        \n\nArchives zip: {ZIP_ARCHIVES}
-        \n\nArchives gz: {GZ_ARCHIVES}
-        \n\nArchives tar: {TAR_ARCHIVES}
+    result = f'''Images jpeg: {[JPEG_IMAGES_NAME]}
+        \n\nImages jpg: {JPG_IMAGES_NAME}
+        \n\nImages svg: {SVG_IMAGES_NAME}
+        \n\nImages png: {PNG_IMAGES_NAME}
+        \n\nAudio mp3: {MP3_AUDIO_NAME}
+        \n\nAudio ogg: {OGG_AUDIO_NAME}
+        \n\nAudio wav: {WAV_AUDIO_NAME}
+        \n\nAudio amr: {AMR_AUDIO_NAME}
+        \n\nVideo avi: {AVI_VIDEO_NAME}
+        \n\nVideo mp4: {MP4_VIDEO_NAME}
+        \n\nVideo mov: {MOV_VIDEO_NAME}
+        \n\nVideo mkv: {MKV_VIDEO_NAME}
+        \n\nDokument doc: {DOC_DOCUMENTS_NAME}
+        \n\nDokument docx: {DOCX_DOCUMENTS_NAME}
+        \n\nDokument txt: {TXT_DOCUMENTS_NAME}
+        \n\nDokument pdf: {PDF_DOCUMENTS_NAME}
+        \n\nDokument xlsx: {XLSX_DOCUMENTS_NAME}
+        \n\nDokument pptx: {PPTX_DOCUMENTS_NAME}
+        \n\nArchives zip: {ZIP_ARCHIVES_NAME}
+        \n\nArchives gz: {GZ_ARCHIVES_NAME}
+        \n\nArchives tar: {TAR_ARCHIVES_NAME}
         \n\nTypes of files in folder: {EXTENSIONS}
         \n\nUnknown files of types: {UNKNOWN}
-        \n\nOthers file: {MY_OTHER}
-        \n\nFolders: {FOLDERS[::-1]}'''
+        \n\nOthers file: {MY_OTHER_NAME}
+        \n\nFolders: {FOLDERS_NAME[::-1]}'''
 
     print(result)
 
