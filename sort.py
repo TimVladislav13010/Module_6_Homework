13d558e422c5419364461aa6f860615777845969
TimVladislav13010 <109065019+TimVladislav13010@users.noreply.github.com>
2022-10-24 15:33:25 +0300

    work sort

--- sort.py
+++ sort.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from normalize import normalize
 import sys
 import time
+import shutil
+
 
 #Списки файлів
-
-
 text_file = []
 audio_file = []
 video_file = []
@@ -21,9 +21,10 @@ def sorter(path): # функція сортування файлів
 
     for i in path.iterdir(): # ітерація по файлах та папках за вказаним шляхом
 
-        if i.is_dir(): # якщо папка то заходимо в неї рекурсивно викликаємо функцію
-            folders.append(i.name)
-            sorter(path / i)
+        if i.is_dir(): # якщо папка то заходимо
+            if i.name not in ("archives", "video", "audio", "documents", "images", "other"):
+                folders.append(i.name)
+                sorter(path / i)  # Рекурсія
 
         if i.is_file(): # якщо файл перевіряємо розширення та додаємо інформацію до списків
             path_file = path / i
@@ -47,7 +48,7 @@ def sorter(path): # функція сортування файлів
 
                 photo_file.append(path_file.name)
                 set_suffix_known.add(path_file.suffix)
-                move_file(path_file, PATH / "photo")
+                move_file(path_file, PATH / "images")
 
             elif format_files == ".avi" \
                     or format_files == ".mp4" \
@@ -73,6 +74,7 @@ def sorter(path): # функція сортування файлів
 
                 archives_file.append(path_file.name)
                 set_suffix_known.add(path_file.suffix)
+                move_archive(path_file, PATH / "archives")
 
             elif format_files: # перевірка інших нам невідомих файлів
 
@@ -80,30 +82,71 @@ def sorter(path): # функція сортування файлів
                 set_suffix_unknown.add(path_file.suffix)
                 move_file(path_file, PATH / "others")
 
+    for i in path.iterdir():  # ітерація по файлах та папках за вказаним шляхом
+        if i.is_dir():  # якщо папка то заходимо
+            if i.name not in ("archives", "video", "audio", "documents", "images", "other"):  # якщо не папка то видаляємо
+                delete_folders(Path(i))  # видаляємо папку
+
     return None
 
 
-def move_file(filename: Path, target_folder: Path):
+def move_file(filename: Path, target_folder: Path):  # функція переміщення файлів
     target_folder.mkdir(exist_ok=True, parents=True)
     normalize_name = filename.stem
-    filename.replace(target_folder / (normalize(normalize_name) + str(time.time()) + filename.suffix))
+    filename.replace(target_folder / (normalize(normalize_name) + "_" + str(time.time()) + filename.suffix))
 
+
+def move_archive(filename: Path, target_folder: Path):  # функція розпакування архівів
+    target_folder.mkdir(exist_ok=True, parents=True)
+    folder_for_file = target_folder / (normalize(filename.name.replace(filename.suffix, '') + "_" + str(time.time())))
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(filename.resolve()),
+                              str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        print(f'Це не архів {filename}!')
+        folder_for_file.rmdir()
+        return None
+    filename.unlink()
+
+
+def delete_folders(folder: Path):  # функція для видалення папок
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f'Помилка видалення папки {folder}')
+
+
+# def path_function():
+#     try:
+#         folder = sys.argv[1]
+#     except IndexError:
+#         print('Enter valid path to the folder')
+#     else:
+#         folder_for_scan = Path(folder)
+#         print(f'Start in folder {folder_for_scan.resolve()}')
+#         main(folder_for_scan.resolve())
+#
+#
+# if __name__ == '__main__':
+#     path_function()
+#
 
 if __name__ == "__main__":
-    pass
+    # pass
 
 
-PATH = sys.argv[1]
-PATH = Path(PATH)
-sorter(PATH)
+    PATH = sys.argv[1]
+    PATH = Path(PATH)
+    sorter(PATH)
 
 
-print(f"""Текстові файли: {text_file}")
-\n\nФайли архівів: {archives_file}
-\n\nАудіо файли: {audio_file}
-\n\nВідео файли: {video_file}
-\n\nФайли зображень: {photo_file}
-\n\nНевідомі файли: {others_file}
-\n\nУсі відомі розширення: {set_suffix_known}
-\n\nУсі невідомі розширення: {set_suffix_unknown}
-\n\nУсі папки: {folders}""")
+    print(f"""Текстові файли: {text_file}")
+    \n\nФайли архівів: {archives_file}
+    \n\nАудіо файли: {audio_file}
+    \n\nВідео файли: {video_file}
+    \n\nФайли зображень: {photo_file}
+    \n\nНевідомі файли: {others_file}
+    \n\nУсі відомі розширення: {set_suffix_known}
+    \n\nУсі невідомі розширення: {set_suffix_unknown}
+    \n\nУсі папки: {folders}""")
